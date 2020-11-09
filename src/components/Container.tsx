00ef5945130226fb24a78f3ff37ea4283b29cefd
ipf-shhyung <shhyung@spindlebooks.com>
2020-11-09 10:15:46 +0900

    Implement [Container] Add Container component

---
+++ src/components/Container.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+
+type Props = {
+  width?: string;
+  height?: string;
+  style?: React.CSSProperties;
+  children: any;
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+  width: ${(props: Props) =>
+    props.width?.indexOf("px") !== -1 || props.width?.indexOf("%") !== -1
+      ? props.width
+      : props.width + "px"};
+  height: ${(props: Props) =>
+    props.height?.indexOf("px") !== -1 || props.height?.indexOf("%") !== -1
+      ? props.height
+      : props.height + "px"};
+  margin: auto;
+`;
+
+function Container({
+  width = "100%",
+  height = "100%",
+  style,
+  children,
+}: Props) {
+  console.log(style);
+  return (
+    <Wrapper width={width} height={height} style={style}>
+      {children}
+    </Wrapper>
+  );
+}
+
+export default Container;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+import Container from "../components/Container";
+
+function Home() {
+  return (
+    <Container>
+      <p>Here</p>
+    </Container>
+  );
+}
+
+export default Home;
