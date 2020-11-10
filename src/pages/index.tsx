cd1617090e7eb9632461c5567be97809eb2c535c
ipf-shhyung <shhyung@spindlebooks.com>
2020-11-10 17:40:58 +0900

    Config [] Add global styles with plain css so that gatsby page links contains the global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 
 import "../layouts/index.css";
-import GlobalStyles from "../layouts/global";
 
-import Header from "../components/Header";
 import Home from "./Home";
 
 function App() {
   return (
     <React.StrictMode>
-      <GlobalStyles />
-      <Header />
       <Home />
     </React.StrictMode>
   );
