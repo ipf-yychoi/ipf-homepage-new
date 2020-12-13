32bf21b0af1e5801da3d30b0c933ae29890e9b0f
ipf-shhyung <shhyung@spindlebooks.com>
2020-12-14 00:31:50 +0900

    Implement [Job] Display Job detail information (in-progress)

--- src/api/getJobsData.tsx
+++ src/api/getJobsData.tsx
@@ -1,9 +1,11 @@
 export function getJobsListData() {
-    return fetch(
-      `https://culture.iportfolio.co.kr/api/homepage/career`
-    ).then((response) => response.json());
-  }
-  
-  export function getJobsDetailsData() {
-    https://culture.iportfolio.co.kr/api/doc/{details}
-  }
+  return fetch(
+    `https://culture.iportfolio.co.kr/api/homepage/career`
+  ).then((response) => response.json());
+}
+
+export function getJobDetail(details: string) {
+  return fetch(
+    `https://culture.iportfolio.co.kr/api/doc/${details}`
+  ).then((response) => response.json());
+}
