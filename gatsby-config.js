4de75ec445ecfa0afcfd4e52141e8fb25a504f02
ipf-sglee <sglee@iportfolio.co.kr>
2021-06-10 15:38:12 +0900

    Fix [] 폰트 깨지는 문제 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 const myCustomQueries = {
-  mobile: "(max-width: 767px)",
-  tablet: "(max-width: 1040px)",
-  desktop: "(min-width: 1040px)",
+  mobile: '(max-width: 767px)',
+  tablet: '(max-width: 1040px)',
+  desktop: '(min-width: 1040px)',
 };
 
 module.exports = {
@@ -14,6 +14,7 @@ module.exports = {
     `gatsby-plugin-react-helmet-async`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-cname`,
+    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -22,7 +23,7 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-plugin-react-svg",
+      resolve: 'gatsby-plugin-react-svg',
       options: {
         rule: {
           include: /.*\.svg/,
@@ -58,7 +59,7 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "G-3ZMK5YY5XD", // Google Analytics / GA
+          'G-3ZMK5YY5XD', // Google Analytics / GA
         ],
         pluginConfig: {
           head: true,
