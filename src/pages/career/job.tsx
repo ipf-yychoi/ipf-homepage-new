baad010693355e73dbffde02a505ccf604ebf01f
ipf-yychoi <97017196+ipf-yychoi@users.noreply.github.com>
2023-07-27 16:40:02 +0900

    Implement [IPFHOME-81] 채용 공고 상세 > 리스트 아이템 마커 제대로 표시되도록 수정 (#58)
    * Config [] yarn.lock 업데이트
    
    * Implement [IPFHOME-81] depth=4인 일반텍스트에 엔터가 포함될 경우 li로 구분되도록 변경
    
    * Implement [IPFHOME-81] 리스트 아이템간 마진 변경

--- src/pages/career/job.tsx
+++ src/pages/career/job.tsx
@@ -87,6 +87,12 @@ const Body3 = styled.p`
   white-space: pre-line;
 `;
 
+const Body3Li = styled(Body3).attrs({ as: 'li' })`
+  &:not(:last-child) {
+    margin-bottom: 0.4rem;
+  }
+`;
+
 const DiscList = styled.ul`
   list-style: disc;
   list-style-position: inside;
@@ -139,7 +145,13 @@ function displayJobDetail(jobsDetailData: [JobDetailType]) {
           <DiscList key={`${jobDetail.title.replace('\n', '')}_${index}`}>
             <Heading4>{jobDetail.title}</Heading4>
             <CircleList>
-              <Body3 as="li">{jobDetail.content}</Body3>
+              {jobDetail.content.split('\n').map((value, index, arr) => {
+                if (index === arr.length - 1) {
+                  return null;
+                }
+
+                return <Body3Li key={value}>{value}</Body3Li>;
+              })}
             </CircleList>
           </DiscList>
         );
